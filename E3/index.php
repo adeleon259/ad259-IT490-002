2bb3be84de0ad287cb7b18f99d602f178b502247
adeleon259 <ad259@njit.edu>
2025-02-19 22:53:58 -0500

    Update index.php

--- E3/index.php
+++ E3/index.php
@@ -5,29 +5,36 @@
         function handleLoginResponse(response) {
             var data = JSON.parse(response); // Parse the JSON response
 
-            if (data.success) {
+            if (data.returnCode === 'success') {
                 // If login is successful, redirect to home.php
-                window.location.href = data.redirect;
+                window.location.href = 'home.php';
             } else {
                 // Display the error message
-                document.getElementById("textResponse").innerHTML = data.error;
+                document.getElementById("textResponse").innerHTML = data.message;
             }
         }
 
-        // Function to send login request
+        // Function to send login request to RabbitMQ
         function sendLoginRequest(username, password) {
-            var request = new XMLHttpRequest(); //Allows browser to make request to server without refreshing the page
+            var request = new XMLHttpRequest(); // Create a new XMLHttpRequest
             request.open("POST", "login.php", true); // Open a POST request to login.php
-            request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); //The data is sent as key=value pairs
-            
-            request.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
+            request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            request.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status === 200) {
                     handleLoginResponse(this.responseText); // Handle the response when received
                 }
             };
-            
-            // Send username and password to login.php
-            request.send("uname=" + username + "&pword=" + password);
+
+            // Send the request data to RabbitMQ
+            var requestData = {
+                type: 'login',
+                username: username,
+                password: password
+            };
+
+            // Send the data as a JSON-encoded string
+            request.send(JSON.stringify(requestData));
         }
     </script>
 </head>
@@ -39,21 +46,8 @@
     
     <!-- Login Form -->
     <form onsubmit="event.preventDefault(); sendLoginRequest(document.getElementById('username').value, document.getElementById('password').value);">
-        <input type="text" id="username" placeholder="Username" required><br>
-        <input type="password" id="password" placeholder="Password" required><br>
+        <input type="text" id="username" placeholder="Enter Username" required><br>
+        <input type="password" id="password" placeholder="Enter Password" required><br>
         <button type="submit">Login</button>
     </form>
 </body>
-</html>
-    
-    <!-- Display login response messages -->
-    <div id="textResponse">Please Enter Name and Password</div>
-    
-    <!-- Login Form -->
-    <form onsubmit="event.preventDefault(); sendLoginRequest(document.getElementById('username').value, document.getElementById('password').value);">
-        <input type="text" id="username" placeholder="Username" required><br>
-        <input type="password" id="password" placeholder="Password" required><br>
-        <button type="submit">Login</button>
-    </form>
-</body>
-</html>
