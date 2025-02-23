83cb76855ad57c90fc11c39490e1f07cbcb92fea
adeleon259 <ad259@njit.edu>
2025-02-23 13:37:29 -0500

    Create testRabbitMQServer.php

---
+++ testRabbitMQServer.php
@@ -0,0 +1,34 @@
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('login.php.inc');
+
+function doLogin($username, $password)
+{
+    // lookup username in database
+    // check password
+    $login = new loginDB();  // Assuming loginDB is a class that interacts with your DB
+    return $login->validateLogin($username, $password);  // Returns true/false
+}
+
+function requestProcessor($request)
+{
+    echo "received request" . PHP_EOL;
+    var_dump($request);
+    if (!isset($request['type'])) {
+        return "ERROR: unsupported message type";
+    }
+    switch ($request['type']) {
+        case "login":
+            return doLogin($request['username'], $request['password']);
+        case "validate_session":
+            return doValidate($request['sessionId']);
+    }
+    return array("returnCode" => '0', 'message' => "Server received request and processed");
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini", "testServer");
+$server->process_requests('requestProcessor');
+exit();
+?>
