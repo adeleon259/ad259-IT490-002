b64adb79edfb6d2a18e434bb0f9e8579d551c35e
adeleon259 <ad259@njit.edu>
2025-02-23 13:12:53 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,47 +1,23 @@
 <?php
-session_start();
+require_once('rabbitMQLib.inc');
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    require_once('rabbitMQLib.inc');
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    
-    $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-    
-    $request = array();
-    $request['type'] = "login";
-    $request['username'] = $username;
-    $request['password'] = $password;
-    
-    $response = $client->send_request($request);
-    
-    if ($response['returnCode'] == 'success') {
-        $_SESSION['user'] = $username;
-        header("Location: dashboard.php"); // Redirect on success
-        exit();
-    } else {
-        $error = "Login failed. Please check your credentials.";
-    }
+$request = array();
+$request['type'] = "login";
+$request['username'] = $username;
+$request['password'] = $password;
+
+$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+$response = $client->send_request($request);
+
+if ($response['returnCode'] == "success") {
+    session_start();
+    $_SESSION['username'] = $username;
+    $_SESSION['session_token'] = $response['session_token'];
+    echo "Login successful!";
+} else {
+    echo "Login failed: " . $response['message'];
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-    <h2>Login</h2>
-    <?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
-    <form method="POST">
-        <label>Username:</label>
-        <input type="text" name="username" required>
-        <br>
-        <label>Password:</label>
-        <input type="password" name="password" required>
-        <br>
-        <input type="submit" value="Login">
-    </form>
-</body>
-</html>
