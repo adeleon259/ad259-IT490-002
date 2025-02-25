f567c78187400b017dce4747a900e6add089618f
adeleon259 <ad259@njit.edu>
2025-02-25 11:37:01 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
@@ -10,19 +13,44 @@ if (!isset($_POST['uname']) || !isset($_POST['pword'])) {
     exit();
 }
 
+$username = $_POST['uname'];
+$password = $_POST['pword'];
+
+echo "Received login request for user: " . $username . "<br>";
+
 // Create a new RabbitMQ client
-$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+try {
+    $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+    if (!$client) {
+        throw new Exception("Failed to create RabbitMQ client.");
+    }
+} catch (Exception $e) {
+    echo json_encode(array("returnCode" => "error", "message" => "RabbitMQ client error: " . $e->getMessage()));
+    exit();
+}
 
 // Prepare the request
 $request = array();
 $request['type'] = "login";
-$request['username'] = $_POST['uname'];
-$request['password'] = $_POST['pword'];
+$request['username'] = $username;
+$request['password'] = $password;
 
 // Send request to RabbitMQ server
-$response = $client->send_request($request);
+try {
+    $response = $client->send_request($request);
 
-// Return the response as JSON
+    if (!$response) {
+        throw new Exception("No response from RabbitMQ.");
+    }
+} catch (Exception $e) {
+    echo json_encode(array("returnCode" => "error", "message" => "RabbitMQ request error: " . $e->getMessage()));
+    exit();
+}
+
+// Debugging output: show response from RabbitMQ
+echo "Response from RabbitMQ: ";
+print_r($response);
+
 echo json_encode($response);
 exit();
 ?>
