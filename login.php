bcd449b887931bd8f13e99fcd4e319778a69679a
adeleon259 <ad259@njit.edu>
2025-02-25 11:11:07 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,37 +1,33 @@
-<html>
-<head>
-    <script>
-        function HandleLoginResponse(response) {
-            try {
-                var text = JSON.parse(response);
-                document.getElementById("textResponse").innerHTML = "Response: " + text.message + "<p>";
-            } catch (error) {
-                document.getElementById("textResponse").innerHTML = "Error processing response.";
-            }
-        }
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
-        function SendLoginRequest(username, password) {
-            var request = new XMLHttpRequest();
-            request.open("POST", "login.php", true);
-            request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+if (!isset($_POST["type"]) || $_POST["type"] !== "login") {
+    echo json_encode(["error" => "Invalid request type"]);
+    exit();
+}
 
-            request.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    HandleLoginResponse(this.responseText);
-                }
-            };
-            
-            request.send("type=login&uname=" + encodeURIComponent(username) + "&pword=" + encodeURIComponent(password));
-        }
-    </script>
-</head>
-<body>
-    <h1>Login Page</h1>
-    <div id="textResponse">Awaiting response...</div>
+// Capture username and password from the frontend
+$username = $_POST["uname"];
+$password = $_POST["pword"];
 
-    <script>
-        SendLoginRequest("kehoed", "12345");
-    </script>
-</body>
-</html>
+$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+
+$request = array();
+$request['type'] = "login";
+$request['username'] = $username;
+$request['password'] = $password;
+
+$response = $client->send_request($request);
+
+// Ensure response is properly formatted
+if (!is_array($response)) {
+    echo json_encode(["error" => "Invalid server response"]);
+    exit();
+}
+
+echo json_encode($response);
+exit();
+?>
 
