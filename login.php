34cc459f72ca5ab2d40ee9f78def018a2c902f8c
adeleon259 <ad259@njit.edu>
2025-02-27 11:12:22 -0500

    Update login.php

--- login.php
+++ login.php
@@ -3,43 +3,31 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-// Function to send login data to RabbitMQ
-function sendToRabbitMQ($username, $password)
-{
-    $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-
-    // Send login request to RabbitMQ
-    $request = array();
-    $request['type'] = "login";
-    $request['username'] = $username;
-    $request['password'] = $password;
-
-    // Get response from RabbitMQ
-    $response = $client->send_request($request);
-
-    return $response;
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode(['returnCode' => 'failure', 'message' => 'Invalid request']);
+    exit;
 }
 
-// Handle the POST request
-$request = json_decode(file_get_contents('php://input'), true);
+// Get user input
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-if (isset($request['username']) && isset($request['password'])) {
-    // Get the login details from the request
-    $username = $request['username'];
-    $password = $request['password'];
+// Create RabbitMQ client instance
+$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
-    // Send the credentials to RabbitMQ and get the response
-    $response = sendToRabbitMQ($username, $password);
+// Prepare request for RabbitMQ
+$request = [
+    "type" => "login",
+    "username" => $username,
+    "password" => $password
+];
 
-    // Check if the login was successful or failed
-    if ($response['returnCode'] === 'success') {
-        echo json_encode(array('returnCode' => 'success', 'message' => 'Login successful'));
-    } else {
-        echo json_encode(array('returnCode' => 'failure', 'message' => 'Login failed'));
-    }
-} else {
-    echo json_encode(array('returnCode' => 'failure', 'message' => 'Invalid request'));
-}
+// Send request and get response
+$response = $client->send_request($request);
+
+// Log response for debugging
+error_log("RabbitMQ response: " . json_encode($response));
+
+// Send response back to frontend
+echo json_encode($response);
 ?>
-
-
